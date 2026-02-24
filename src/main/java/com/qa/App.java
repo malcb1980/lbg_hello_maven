fb68c6456551ec1cb38f7716c184de2049b58e9e
Mal <Malcolmbell17@gmail.com>
2026-02-24 14:28:56 +0000

    EDIT: Added Olly & changed version to 2.0 and removed Snapshot

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -14,6 +14,7 @@ public class App
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
          System.out.println(sayHelloToSomeone("Mal"));
+         System.out.println(sayHelloToSomeone("Olly"));
         System.out.println(sayGoodbye());
     }
 
